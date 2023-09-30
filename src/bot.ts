c082d137be268f81260246d985b107b663261623
aeither <36173828+aeither@users.noreply.github.com>
2023-09-30 21:10:35 +0400

    simplify

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,5 @@
 import { TELEGRAM_BOT_TOKEN } from '../lib/constants.ts'
-import { Bot, Context, InlineKeyboard, Keyboard } from './deps.ts'
+import { Bot, Keyboard } from './deps.ts'
 
 export const bot = new Bot(TELEGRAM_BOT_TOKEN, {
   client: {
@@ -21,20 +21,14 @@ const WEBAPP_URL =
     : 'https://calorie-counter-jade.vercel.app'
 console.log('WEBAPP_URL', WEBAPP_URL)
 
-// const inlineKeyboard = new InlineKeyboard()
-// inlineKeyboard.webApp('Calorie Counter', WEBAPP_URL)
-
 const keyboard = new Keyboard()
 keyboard.webApp('Calorie Counter', WEBAPP_URL)
 
-bot.command('start', (ctx) => ctx.reply('Welcome!'))
-
-bot.command('app', (ctx) => ctx.reply('Open App', { reply_markup: keyboard }))
-
-bot.command('ping', (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`))
+bot.command('start', (ctx) => ctx.reply('Welcome!', { reply_markup: keyboard }))
 
 bot.filter(
-  (ctx) => ctx.update.message !== undefined && ctx.update.message.web_app_data !== undefined,
+  (ctx) =>
+    ctx.update.message !== undefined && ctx.update.message.web_app_data !== undefined,
   (ctx) => {
     console.log('web_app_message: ')
     console.log(ctx.update.message?.web_app_data)
